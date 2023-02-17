f8151ed314915f6bdee5c4205bf5e2dffa4ad032
Victor Ike <victorike08@gmail.com>
2023-02-16 20:47:43 -0600

    Update JS contents

--- script.js
+++ script.js
@@ -3,31 +3,23 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable no-console */
 
-let modalBtn = document.getElementById('modal-btn');
-let modal = document.querySelector('.modal');
-let closeBtn = document.querySelector('.close-btn');
+// Get and display modal information
+const modalBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-btn');
 
-modalBtn.onclick = function (e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
-  }
+modalBtn.addEventListener('click', () => {
   modal.style.display = 'block';
-};
-closeBtn.onclick = function () {
+});
+
+closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
-};
-// window.onclick = function(e) {
-//   if (e.target == modal) {
-//     modal.style.display = "none"
-//   }
-// }
+});
 
-// TO DO
-
-//Create functions and addEventlisteners to the modal
-
+// Start Library Logic
 let myLibrary = [];
 
+// Book constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -39,15 +31,46 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  let book = new Book(title, author, pages, read);
-  myLibrary.push(book);
+  let books = new Book(title, author, pages, read);
+  myLibrary.push(books);
 }
 
-const bookOne = addBookToLibrary(
-  'Basic Economics',
-  'Thomas Sowell',
-  912,
-  'reading'
-);
-console.log(myLibrary);
-console.log(bookOne);
+/* const bookOne = addBookToLibrary(
+    'Basic Economics',
+    'Thomas Sowell',
+    912,
+    'reading'
+    );
+    const bookTwo = addBookToLibrary(
+      'Basic Economics',
+      'Thomas Sowell',
+      912,
+      'reading'
+      ); */
+
+// Display books to page
+function displayBooks() {
+  const books = document.querySelector('.books');
+
+  // Loop over the library array and display to the table
+  myLibrary.forEach((library) => {});
+}
+
+// Add form inputs to array
+const submitBtn = document.querySelector('#add-btn');
+submitBtn.addEventListener('click', getFormData);
+
+function getFormData() {
+  const title = document.querySelector('#book-title').value;
+  const author = document.querySelector('#book-author').value;
+  const pages = document.querySelector('#book-pages').value;
+  const read = document.querySelector('#read-status').value;
+  console.log(title, author, pages, read);
+
+  // AddLibrary function take add input data to array
+  addBookToLibrary(title, author, pages, read);
+
+  // Clear out from after
+  document.querySelector('#myform').reset();
+}
+getFormData();
