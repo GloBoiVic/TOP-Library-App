262ae60fa79ac711165ea041fd29f3b19ddf0e90
Victor Ike <victorike08@gmail.com>
2023-02-26 14:52:07 -0600

    Update JS Contents

--- script.js
+++ script.js
@@ -31,6 +31,10 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+  this.read = this.read === 'Read' ? 'Unread' : 'Read';
+};
+
 // Get form info from the DOM and push book to library array
 function addBookToLibrary() {
   const title = document.querySelector('#book-title').value;
@@ -42,12 +46,15 @@ function addBookToLibrary() {
   // Break out of form if empty
   if (title === '' || author === '' || pages === '') {
     errorMsg.textContent = 'Please fill out all fields';
-    // modal.style.display = 'none';
   } else {
     const newBook = new Book(title, author, pages, read);
+    newBook.toggleRead();
     myLibrary.push(newBook);
     displayBooks();
     errorMsg.textContent = '';
+
+    // Clear out input everytime modal pops up
+    form.reset();
   }
 }
 
@@ -56,76 +63,83 @@ const form = document.querySelector('#myform');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addBookToLibrary();
-  // Clear existing values in array everytime form submits
-  myLibrary = [];
-  // Clear out form everytime modal pops up
-  form.reset();
 });
 
-// Display books in table format
 function displayBooks() {
   const table = document.querySelector('table');
   const tblBody = document.querySelector('tbody');
-  // Loop over the library array and display to the table
-  myLibrary.forEach((library) => {
-    console.log(library.title);
-    const tblRow = document.createElement('tr');
-    tblRow.dataset.delete = 'removebooks';
 
-    let tblCell1 = document.createElement('td');
-    tblCell1.textContent = library.title;
-    tblRow.appendChild(tblCell1);
+  // Clear existing books from the page
+  tblBody.innerHTML = '';
 
-    const tblCell2 = document.createElement('td');
-    tblCell2.textContent = library.author;
-    tblRow.appendChild(tblCell2);
-
-    const tblCell3 = document.createElement('td');
-    tblCell3.textContent = library.pages;
-    tblRow.appendChild(tblCell3);
-
-    const tblCell4 = document.createElement('td');
-    const readBtn = document.createElement('button');
-    readBtn.textContent = library.read;
-    readBtn.classList.add('read-btn');
-    tblCell4.appendChild(readBtn);
-    tblRow.appendChild(tblCell4);
-
-    const tblCell5 = document.createElement('td');
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.addEventListener('click', removeBook);
-    tblCell5.appendChild(deleteBtn);
-    tblRow.appendChild(tblCell5);
-
-    tblBody.appendChild(tblRow);
-    table.appendChild(tblBody);
-
-    // Goes back to the home screen after each successful add
-    modal.style.display = 'none';
+  // Loop over the library array and append each book to the table
+  myLibrary.forEach((book, index) => {
+    tblBody.appendChild(renderBook(book, index));
   });
+
+  table.appendChild(tblBody);
+
+  // Goes back to the home screen after each successful add
+  modal.style.display = 'none';
 }
+
+function renderBook(book, index) {
+  const tblRow = document.createElement('tr');
+  tblRow.dataset.delete = 'removebooks';
+
+  let tblCell1 = document.createElement('td');
+  tblCell1.textContent = book.title;
+  tblRow.appendChild(tblCell1);
+
+  const tblCell2 = document.createElement('td');
+  tblCell2.textContent = book.author;
+  tblRow.appendChild(tblCell2);
+
+  const tblCell3 = document.createElement('td');
+  tblCell3.textContent = book.pages;
+  tblRow.appendChild(tblCell3);
+
+  const tblCell4 = document.createElement('td');
+  const readBtn = document.createElement('button');
+  readBtn.textContent = book.read;
+  readBtn.classList.add('read-btn');
+  readBtn.addEventListener('click', () => {
+    book.toggleRead();
+    readBtn.textContent = book.read;
+  });
+  tblCell4.appendChild(readBtn);
+  tblRow.appendChild(tblCell4);
+
+  const tblCell5 = document.createElement('td');
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.value = index;
+  deleteBtn.addEventListener('click', removeBook);
+  tblCell5.appendChild(deleteBtn);
+  tblRow.appendChild(tblCell5);
+
+  return tblRow;
+}
+
+// Adds default books to the page
+function addDefaultBooks() {
+  const book1 = new Book('Think and Grow Rich', 'Napoleon Hill', 397, 'Unread');
+  const book2 = new Book('Basic Economics', 'Thomas Sowell', 920, 'Read');
+  myLibrary.push(book1, book2);
+}
+
+// Call addDefaultBooks before displaying books
+addDefaultBooks();
+displayBooks();
 
 // Function to remove book when delete btn is clicked
-function removeBook() {
+function removeBook(event) {
   const tblBody = document.querySelector('tbody');
   const libraryList = tblBody.children;
-  for (list of libraryList) {
-    if (list.hasAttribute('data-delete')) {
-      list.remove();
-    }
-  }
+  const index = Array.from(libraryList).indexOf(
+    event.target.parentNode.parentNode
+  );
+  tblBody.removeChild(libraryList[index]);
+  myLibrary.splice(index, 1);
 }
-
-// If I loop through through html collections using this method, it deletes all the book instead of 1
-/* function removeBook() {
-  const tblBody = document.querySelector('tbody');
-  const libraryList = tblBody.children;
-  console.log(libraryList);
-  Array.from(libraryList).forEach((list, index) => {
-    if (list.hasAttribute('data-delete')) {
-      list.remove(index);
-    }
-  });
-} */
